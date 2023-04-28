6795a8d5087da1faeef1de5422876f3128a9482e
sammkrt <samet.krt@gmail.com>
2023-04-28 16:08:37 +0200

    Add Contact form popUp

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,139 +1,160 @@
-import React, { useRef, useState } from "react";
-import emailjs from '@emailjs/browser';
+import React, { useEffect, useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
 export default function Contact() {
-
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
+  const [successMessage, setSuccessMessage] = useState(false);
+  const form = useRef<HTMLFormElement>(null);
 
-  function encode(data: any) {
-    return Object.keys(data)
-      .map(
-        (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
-      )
-      .join("&");
-  }
+  useEffect(() => {
+    if (successMessage) {
+      setTimeout(() => {
+        setSuccessMessage(false);
+      }, 2000);
+    }
+  }, [successMessage]);
 
-  // function handleSubmit(e : any) {
-  //   e.preventDefault();
-  //   fetch("/", {
-  //     method: "POST",
-  //     headers: { "Content-Type": "application/x-www-form-urlencoded" },
-  //     body: encode({ "form-name": "contact", name, email, message }),
-  //   })
-  //     .then(() => alert("Message sent!"))
-  //     .catch((error) => alert(error));
-  // }
-  
-    const form = useRef<HTMLFormElement>(null);
-    
-    const sendEmail = (e : any) => {
-      e.preventDefault(); // prevents the page from reloading when you hit “Send”
-      const formRef = form.current;
-      if (formRef) {
-        emailjs.sendForm('service_l5920mn', 'contact_form', form.current, 'kn2zt2xprnI-8lEUf')
-          .then((result) => {
-            alert("Message sent")
-          }, (error) => {
+  const sendEmail = (e: any) => {
+    e.preventDefault(); // prevents the page from reloading when you hit “Send”
+    const formRef = form.current;
+    if (formRef) {
+      emailjs
+        .sendForm(
+          "service_l5920mn",
+          "contact_form",
+          form.current,
+          "kn2zt2xprnI-8lEUf"
+        )
+        .then(
+          (result) => {
+            setSuccessMessage(true);
+          },
+          (error) => {
             // show the user an error
-          });
-      }
-    };
-  
-    return (
-      <section id="contact" className="relative">
-        <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
-          <div className="lg:w-2/3 md:w-1/2 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
-            <iframe
-              width="100%"
-              height="100%"
-              title="map"
-              className="absolute inset-0"
-              frameBorder={0}
-              marginHeight={0}
-              marginWidth={0}
-              style={{ filter: "opacity(0.7)" }}
-              src="https://www.google.com/maps/embed/v1/place?q=De+Pijp,+Amsterdam,+Netherlands&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
-            />
-            <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
-              <div className="lg:w-1/2 px-6">
-                <h2 className="title-font font-semibold text-white tracking-widest text-xs">
-                  ADDRESS
-                </h2>
-                <p className="mt-1">
-                  De Pijp <br />
-                  Amsterdam, Netherlands
-                </p>
-              </div>
-              <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
-                <h2 className="title-font font-semibold text-white tracking-widest text-xs">
-                  EMAIL
-                </h2>
-             
-                <a
-                className="text-indigo-400 leading-relaxed">
-                </a>
+          }
+        );
+    }
+  };
+
+  return (
+    <section id="contact" className="relative">
+        {successMessage && (
+        <div className="fixed z-50 inset-0 overflow-y-auto">
+          <div className="flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+            <div className="fixed inset-0 transition-opacity">
+              <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
+            </div>
+            <span className="hidden sm:inline-block sm:align-middle sm:h-screen"></span>
+            &#8203;
+            <div
+              className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full"
+              role="dialog"
+              aria-modal="true"
+              aria-labelledby="modal-headline"
+            >
+              <div className="bg-blue-100 border-t border-b border-blue-500 text-blue-700 px-4 py-3">
+                <p className="font-bold">Informational message</p>
+                <p className="text-sm">Message sent</p>
               </div>
             </div>
           </div>
-          <form
-            onSubmit={sendEmail}
-            ref={form}
-            name="contact"
-            method="POST"
-            data-netlify="true"
-            className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
-            <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
-              Hire Me
-            </h2>
-            <p className="leading-relaxed mb-5">
-              Don't hesitate to contact me if you have any questions or would like to discuss potential opportunities further.
-            </p>
-
-            <div className="relative mb-4">
-              <label htmlFor="name" className="leading-7 text-sm text-gray-400">
-                Name
-              </label>
-              <input
-                type="text"
-                id="name"
-                name="name"
-                className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                onChange={(e) => setName(e.target.value)}
-              />
-            </div>
-            <div className="relative mb-4">
-              <label htmlFor="email" className="leading-7 text-sm text-gray-400">
-                Email
-              </label>
-              <input
-                type="email"
-                id="email"
-                name="email"
-                className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </div>
-            <div className="relative mb-4">
-              <label
-                htmlFor="message"
-                className="leading-7 text-sm text-gray-400">
-                Message
-              </label>
-              <textarea
-                id="message"
-                name="message"
-                className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
-                onChange={(e) => setMessage(e.target.value)}
-              />
-            </div>
-            <button
-              type="submit"
-              className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
-              Submit
-            </button>
-          </form>
         </div>
-      </section>
-    );
+      )}
+      <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
+        <div className="lg:w-2/3 md:w-1/2 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
+          <iframe
+            width="100%"
+            height="100%"
+            title="map"
+            className="absolute inset-0"
+            frameBorder={0}
+            marginHeight={0}
+            marginWidth={0}
+            style={{ filter: "opacity(0.7)" }}
+            src="https://www.google.com/maps/embed/v1/place?q=De+Pijp,+Amsterdam,+Netherlands&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
+          />
+          <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
+            <div className="lg:w-1/2 px-6">
+              <h2 className="title-font font-semibold text-white tracking-widest text-xs">
+                ADDRESS
+              </h2>
+              <p className="mt-1">
+                De Pijp <br />
+                Amsterdam, Netherlands
+              </p>
+            </div>
+            <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
+              <h2 className="title-font font-semibold text-white tracking-widest text-xs">
+                EMAIL
+              </h2>
+
+              <a className="text-indigo-400 leading-relaxed"></a>
+            </div>
+          </div>
+        </div>
+        <form
+          onSubmit={sendEmail}
+          ref={form}
+          name="contact"
+          method="POST"
+          data-netlify="true"
+          className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0"
+        >
+          <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
+            Hire Me
+          </h2>
+          <p className="leading-relaxed mb-5">
+            Don't hesitate to contact me if you have any questions or would like
+            to discuss potential opportunities further.
+          </p>
+
+          <div className="relative mb-4">
+            <label htmlFor="name" className="leading-7 text-sm text-gray-400">
+              Name
+            </label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+          <div className="relative mb-4">
+            <label htmlFor="email" className="leading-7 text-sm text-gray-400">
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="relative mb-4">
+            <label
+              htmlFor="message"
+              className="leading-7 text-sm text-gray-400"
+            >
+              Message
+            </label>
+            <textarea
+              id="message"
+              name="message"
+              className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+              onChange={(e) => setMessage(e.target.value)}
+            />
+          </div>
+          <button
+            type="submit"
+            className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 }
